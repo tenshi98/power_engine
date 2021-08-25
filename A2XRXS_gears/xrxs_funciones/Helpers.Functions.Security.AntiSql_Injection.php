22c666cabd5cae9f13e3fe04d4712e0a300894cf
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-08-25 18:50:02 -0400

    Actualizacion libreria
    Se actualiza la libreria con nuevas funciones

--- A2XRXS_gears/xrxs_funciones/Helpers.Functions.Security.AntiSql_Injection.php
+++ A2XRXS_gears/xrxs_funciones/Helpers.Functions.Security.AntiSql_Injection.php
@@ -16,53 +16,61 @@ function secure($Data) {
 	if (!is_numeric($Data)) {
 		
 		//se definen las letras a reemplazar
-    /*$originales   = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿª';
-    $modificadas  = 'aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybya';
-    $cadena       = utf8_decode($oracion);
-    $cadena       = strtr($cadena, utf8_decode($originales), $modificadas);
-    $oracion      = utf8_encode($cadena);
+		$originales   = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿª';
+		$modificadas  = 'aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybya';
+		$cadena       = utf8_decode($Data);
+		$cadena       = strtr($cadena, utf8_decode($originales), $modificadas);
+		$Data         = utf8_encode($cadena);
+		//se cambian todas las letras a minusculas
+		$Data         = strtolower($Data);
 	
-	//Lista de palabras censuradas en ingles 
-	$censuradas_1 = array('fuck','horny','aroused','hentai','slut','slag','boob','pussy','vagina',
-						'faggot','bugger','bastard','cunt','nigga','nigger','jerk','wanker',
-						'tosser','shit','rape','rapist','dick','cock','whore','bitch','asshole',
-						'twat','titt','piss','intercourse','sperm','spunk','testicle','milf',
-						'retard','anus','dafuq','gay','lesbian','homo','homosexual','cum',
-						'prostitute','wtf','penis','ffs','pedo','hack','dumb','crap','fuck you',
-						'bullshit','damn','hell','ass','badass','son of a bitch','pissed off',
-						'dickhead','motherfucker','dumbass','tramp');
-	
-	
-	DROP
-	TABLE 
-	INSERT 
-	UPDATE 
-	SELECT ; 'OR' "OR"
-	%27
-	%3B
-	--
-	+
-						
-	$partes_1   = count($censuradas_1);					
-	$contador   = 0;
-	
-	
-	for ($i=0; $i < $partes_1; $i++) { 
-		if( strpos($oracion,' '.$censuradas_1[$i].' ') !== false ){
-			//Cuenta las prohibidas
-			$contador++;
+		//%20 = espacio
+		//%22 = "
+		//%25 = %
+		//%27 = '
+		//%2B = +
+		//%2D = -
+		//%2F = /
+		//%3B = ;
+		//%3C = <
+		//%3D = =
+		//%3E = >
+		//%3F = ?
+		
+		//Lista de palabras censuradas en ingles 
+		$inject = array('drop','table','insert','update','select','like', 'union', 'truncate', 'shutdown', 'show', 
+						'"', '""',"'", "''",
+						'";', "';", ';',
+						'"OR"', "'OR'",
+						'char(',
+						'%20', '%22', '%25', '%27', '%2B', '%2D', '%2F', '%3B', '%3C', '%3D', '%3E', '%3F'
+		
+		);
+		
+		//
+		$contador = 0;
+		//se revisa una a una
+		for ($i=0; $i < count($inject); $i++) { 
+			if( strpos($Data,$inject[$i]) !== false ){
+				
+				//Cuenta las prohibidas
+				$contador++;
+			}
 		}
-	}*/						
+		
+		//si no hay ninguna
+		if($contador==0){
+			return $Data;
+		}else{
+			return $contador;
+		}						
 		
 		//$Data = mysqli_real_escape_string($dbConn, $Data);
 	}elseif(is_numeric($Data) && abs($Data) == $Data){
 		$Data = $Data;
-		
+		return $Data;
 	}
-
-
-
-	return $Data;
+	
 }
 function SanitizarDatos($Data) {
 // Loop through POST variables
@@ -71,5 +79,15 @@ function SanitizarDatos($Data) {
 	}
 	return $Data;
 }
-	
+
+
+function anti_injection($sql){
+    $sql = preg_replace(sql_regcase("/(from|select|insert|delete|where|drop table|show tables|#|\*|--|\\\\)/"), "" ,$sql);
+    $sql = trim($sql);
+    $sql = strip_tags($sql);
+    $sql = (get_magic_quotes_gpc()) ? $sql : addslashes($sql);
+    return $sql;
+}
+
+
 ?>
