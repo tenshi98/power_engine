86f13feae4e36dc2228f55ddbcce6ea565a8f6ad
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-09-05 14:06:12 -0400

    Eliminacion dependencias
    Se eliminan dependencias debido a que estas se trasladan al head del documento

--- LIBS_js/tooltipster/tooltipster.php
+++ LIBS_js/tooltipster/tooltipster.php
@@ -1,6 +1,4 @@
 <!-- Burbuja de ayuda -->
-<link href="<?php echo DB_SITE_REPO ?>/LIBS_js/tooltipster/css/tooltipster.bundle.min.css" rel="stylesheet" type="text/css">
-<script src="<?php echo DB_SITE_REPO ?>/LIBS_js/tooltipster/js/tooltipster.bundle.min.js"></script>
 <script>
 	<!--
 	$(document).ready(function() {
