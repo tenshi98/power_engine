5317a0465f9242f3ff506ce23bd7ffa43f292394
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-09-27 16:26:50 -0400

    Modificacion widget
    Se agrega nueva opcion a los existentes, se crea widget de doble eje

--- A2XRXS_gears/xrxs_funciones/Components.UI.Widgets.Graphics.php
+++ A2XRXS_gears/xrxs_funciones/Components.UI.Widgets.Graphics.php
@@ -14,8 +14,12 @@ function GraphLinear_1($idDiv,
 	
 	//Opciones del legend
 	switch ($legendOptions) {
-		//Opcion abajo
+		//Legend abajo
 		case 1:
+			$lopts = 'height: 600,legend: {"orientation": "h",x: 0,  y: -1, bgcolor: "#E2E2E2", bordercolor: "#FFFFFF", borderwidth: 2}';
+			break;
+		//Doble eje
+		case 2:
 			$lopts = 'height: 600,legend: {"orientation": "h",x: 0,  y: -1, bgcolor: "#E2E2E2", bordercolor: "#FFFFFF", borderwidth: 2}';
 			break;
 		default:
@@ -90,8 +94,12 @@ function GraphLinear_2($idDiv,
 	
 	//Opciones del legend
 	switch ($legendOptions) {
-		//Opcion abajo
+		//Legend abajo
 		case 1:
+			$lopts = 'height: 600,legend: {"orientation": "h",x: 0,  y: -1, bgcolor: "#E2E2E2", bordercolor: "#FFFFFF", borderwidth: 2}';
+			break;
+		//Doble eje
+		case 2:
 			$lopts = 'height: 600,legend: {"orientation": "h",x: 0,  y: -1, bgcolor: "#E2E2E2", bordercolor: "#FFFFFF", borderwidth: 2}';
 			break;
 		default:
@@ -148,6 +156,87 @@ function GraphLinear_2($idDiv,
 
 		Plotly.newPlot(\''.$idDiv.'\', dataPlotly, layout, config);
 	</script>';	
+	
+	return $graph;
+	
+}
+/*******************************************************************************************************************/
+//Crea un grafico de doble eje
+function GraphLinear_3($idDiv, 
+						$titulo, $eje_x_titulo, $eje_y_titulo_1, $eje_y_titulo_2, 
+						$xData_1, $yData_1, $name_1,
+						$xData_2, $yData_2, $name_2,
+						$legendOptions){
+	
+	//Opciones del legend
+	switch ($legendOptions) {
+		//Legend abajo
+		case 1:
+			$lopts = 'height: 600,legend: {"orientation": "h",x: 0,  y: -1, bgcolor: "#E2E2E2", bordercolor: "#FFFFFF", borderwidth: 2}';
+			break;
+		//Doble eje
+		case 2:
+			$lopts = 'height: 600,legend: {"orientation": "h",x: 0,  y: -1, bgcolor: "#E2E2E2", bordercolor: "#FFFFFF", borderwidth: 2}';
+			break;
+		default:
+			$lopts = '';
+	}
+
+	/*************************************************/
+	//imprime
+	$graph  = '<div id="'.$idDiv.'"></div>';
+	$graph .= '<script>
+		//guardo las fechas
+		var xData_1 = ['.$xData_1.'];
+		var xData_2 = ['.$xData_2.'];
+		
+		var trace1 = {
+			x: xData_1,
+			y: ['.$yData_1.'],
+			name: '.$name_1.',
+			type: \'scatter\'
+		};
+
+		var trace2 = {
+			x: xData_2,
+			y: ['.$yData_2.'],
+			name: '.$name_2.',
+			yaxis: \'y2\',
+			type: \'scatter\'
+		};
+
+		var dataPlotly = [trace1, trace2];
+		
+		//vista de los label
+		var labelview = true;
+		if(xData_1.length > 30){
+			labelview = false;
+		}
+		if(xData_2.length > 30){
+			labelview = false;
+		}
+			
+		var layout = {';
+			if(isset($titulo)&&$titulo!=''){                  $graph .= 'title:\''.$titulo.'\',';}
+			if(isset($eje_x_titulo)&&$eje_x_titulo!=''){      $graph .= 'xaxis: {title: \''.$eje_x_titulo.'\', showticklabels: labelview},';}
+			if(isset($eje_y_titulo_1)&&$eje_y_titulo_1!=''){  $graph .= 'yaxis: {title: \''.$eje_y_titulo_1.'\'},';}
+			if(isset($eje_y_titulo_2)&&$eje_y_titulo_2!=''){  $graph .= 'yaxis2: {title: \''.$eje_y_titulo_2.'\', titlefont: {color: \'rgb(148, 103, 189)\'}, tickfont: {color: \'rgb(148, 103, 189)\'}, overlaying: \'y\', side: \'right\' },';}
+			$graph .= 'showlegend: true,';
+			$graph .= $lopts;
+		$graph .= ' };
+
+		var config = {
+			locale: \'es-ar\',
+			displayModeBar: true,
+			modeBarButtonsToRemove: [\'select2d\', \'lasso2d\', \'zoom2d\', \'zoomIn2d\', \'zoomOut2d\'],
+			displaylogo: false,
+			responsive: true
+		};
+
+		Plotly.newPlot(\''.$idDiv.'\', dataPlotly, layout, config);
+	</script>
+	';
+			
 	
 	return $graph;
 	
