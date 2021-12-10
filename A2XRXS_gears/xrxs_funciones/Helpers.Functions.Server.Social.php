ff97bb35fb6e02f4cd723ffa06dedc76592b09bf
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2021-12-10 09:30:35 -0400

    Se agregan nuevas funciones
    Se agrega funcion para el envio de mensajes al grupo de whatsapp

--- A2XRXS_gears/xrxs_funciones/Helpers.Functions.Server.Social.php
+++ A2XRXS_gears/xrxs_funciones/Helpers.Functions.Server.Social.php
@@ -30,26 +30,135 @@ if( ! defined('XMBCXRXSKGC')) {
 ************************************************************************/
 //Funcion
 function WhatsappSendMessage($Token, $InstanceId, $Phone, $Body){
-	$data = [
-		'phone' => $Phone, // Receivers phone
-		'body' => $Body, // Message
-	];
-	$json = json_encode($data); // Encode data to JSON
-	// URL for request POST /message
-	$token      = $Token;
-	$instanceId = $InstanceId;
-	$url = 'https://api.chat-api.com/instance'.$instanceId.'/message?token='.$token;
-	// Make a POST request
-	$options = stream_context_create(['http' => [
-			'method'  => 'POST',
-			'header'  => 'Content-type: application/json',
-			'content' => $json
-		]
-	]);
-	// Send a request
-	$result = file_get_contents($url, false, $options);
+	//verifico si numero comienza con +56 o con 56
+	$myNumber = $Phone;
+	$findme_1 = '+';
+	$findme_2 = '+56';
+	$findme_3 = '56';
+	$pos_1      = strpos($myNumber, $findme_1);
+	$pos_2      = strpos($myNumber, $findme_2);
+	$pos_3      = strpos($myNumber, $findme_3);
+		
+	//si comienza con el +
+	if ($pos_1 !== false && $pos_1==0) {
+		//comienza con el +56
+		if ($pos_2 !== false && $pos_2==0) {
+			$myPhone = $Phone;
+		//no comienza con el +56, es otro numero
+		} else {
+			$myPhone = '';
+		}
+	//no comienza por el +
+	} else {
+		//comienza con el 56
+		if ($pos_3 !== false && $pos_3==0) {
+			$myPhone = '+'.$Phone;
+		//no comienza con el 56, es otro numero
+		} else {
+			$myPhone = '+56'.$Phone;
+		}
+	}
 	
-	return $result;
+	/**************************************/
+	//verifico la existencia de datos
+	if(isset($myPhone)&&$myPhone!=''&&isset($InstanceId)&&$InstanceId!=''&&isset($Token)&&$Token!=''){
+		$data = [
+			'phone' => $myPhone, // Receivers phone
+			'body' => $Body, // Message
+		];
+		$json = json_encode($data); // Encode data to JSON
+		// URL for request POST /message
+
+		$url = 'https://api.chat-api.com/instance'.$InstanceId.'/message?token='.$Token;
+		// Make a POST request
+		$options = stream_context_create(['http' => [
+				'method'  => 'POST',
+				'header'  => 'Content-type: application/json',
+				'content' => $json
+			]
+		]);
+		// Send a request
+		$result = file_get_contents($url, false, $options);
+		
+		/*error_log("===============================================", 0);
+		error_log("myPhone:".$myPhone, 0);
+		error_log("InstanceId:".$InstanceId, 0);
+		error_log("Token:".$Token, 0);
+		error_log("url:".$url, 0);
+		error_log("result:".$result, 0);
+		error_log("===============================================", 0);*/
+		
+		//return $result;
+	//guardo el log
+	}else{
+		error_log("===============================================", 0);
+		error_log("myPhone:".$myPhone, 0);
+		error_log("InstanceId:".$InstanceId, 0);
+		error_log("Token:".$Token, 0);
+		error_log("===============================================", 0);
+	}
+	
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************
+* Envio de mensajes a un grupo whatsapp
+* 
+*===========================     Detalles    ===========================
+* Permite el envio de mensajes a grupos de whatsapp a traves de chat-api
+*===========================    Modo de uso  ===========================
+* 	
+* 	//se obtiene dato
+* 	WhatsappGroupSendMessage('asdertcvbtrtr', '356644', 'groupTest', 'test');
+* 
+*===========================    Parametros   ===========================
+* String   $Token        Token de la plataforma
+* String   $InstanceId   Instancia a utilizar
+* String   $Phone        Telefono a enviar el mensaje
+* String   $Body         mensaje
+* @return  Date
+************************************************************************/
+//Funcion
+function WhatsappGroupSendMessage($Token, $InstanceId, $Group, $Body){
+	
+	/**************************************/
+	//verifico la existencia de datos
+	if(isset($Group)&&$Group!=''&&isset($InstanceId)&&$InstanceId!=''&&isset($Token)&&$Token!=''){
+		$data = [
+			'chatId' => $Group, // Receivers phone
+			'body' => $Body, // Message
+		];
+		$json = json_encode($data); // Encode data to JSON
+		// URL for request POST /message
+
+		$url = 'https://api.chat-api.com/instance'.$InstanceId.'/message?token='.$Token;
+		// Make a POST request
+		$options = stream_context_create(['http' => [
+				'method'  => 'POST',
+				'header'  => 'Content-type: application/json',
+				'content' => $json
+			]
+		]);
+		// Send a request
+		$result = file_get_contents($url, false, $options);
+		
+		/*error_log("===============================================", 0);
+		error_log("Group:".$Group, 0);
+		error_log("InstanceId:".$InstanceId, 0);
+		error_log("Token:".$Token, 0);
+		error_log("url:".$url, 0);
+		error_log("result:".$result, 0);
+		error_log("===============================================", 0);*/
+		
+		//return $result;
+	//guardo el log
+	}else{
+		error_log("===============================================", 0);
+		error_log("Group:".$Group, 0);
+		error_log("InstanceId:".$InstanceId, 0);
+		error_log("Token:".$Token, 0);
+		error_log("===============================================", 0);
+	}
+	
 }
 
 ?>
