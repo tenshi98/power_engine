9e6fd0187a6481bceaa1bb707bb9325cb190f22a
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2022-09-08 18:29:35 -0400

    Creacion funcion de encriptacion
    Creacion funcion de encriptacion

--- A2XRXS_gears/xrxs_funciones/Helpers.Functions.Security.Codification.php
+++ A2XRXS_gears/xrxs_funciones/Helpers.Functions.Security.Codification.php
@@ -100,11 +100,60 @@ function simpleDecode($value, $passkey) {
     return $decrypttext;
 }
 
-//Codificacion pripia por cada servidor, esto impide el copiado de informacion entre servidores
+//Codificacion propia por cada servidor, esto impide el copiado de informacion entre servidores
 function generateServerSpecificHash(){
 	return (isset($_SERVER['SERVER_NAME']) && !empty($_SERVER['SERVER_NAME']))
             ? md5($_SERVER['SERVER_NAME'])
             : md5(pathinfo(__FILE__, PATHINFO_FILENAME));
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************
+* Decodifica un texto
+* 
+*===========================     Detalles    ===========================
+* Permite decodificar un texto para que quede legible a la lectura normal,
+* con la opcion de la utilizacion de una palabra clave para su 
+* decodificacion
+*===========================    Modo de uso  ===========================
+* 	
+* 	// Encriptas id 5008   
+* 	$encriptar = encrypt_decrypt('encrypt',5008);
+* 	echo $encriptar . '<br>';
+* 	
+* 	// Desencriptas el id para verlo de manera original
+* 	$decriptar = encrypt_decrypt('decrypt',$encriptar);
+* 	echo $decriptar;
+* 	
+* 	//salidas:
+* 	bnR6UTRVTHAzYWd1dWEvWVdpMGo4QT09 (corresponde a 5008)
+* 	5008 
+* 
+*===========================    Parametros   ===========================
+* String   $string   Texto a transformar
+* String   $passkey  (Opcional)Palabra clave de codificacion
+* @return  String
+************************************************************************/ 
+function encrypt_decrypt($action, $string) :string {
+	$output = false;
+	$encrypt_method = "AES-256-CBC";
+	$secret_key     = 'tu_clave_secreta';
+	$secret_iv      = 'salt_secreto';
+	// hash
+	$key = hash('sha256', $secret_key);    
+	// iv - encrypt method AES-256-CBC expects 16 bytes 
+	$iv = substr(hash('sha256', $secret_iv), 0, 16);
+	if ( $action == 'encrypt' ) {
+		$output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+		$output = base64_encode($output);
+	}elseif( $action == 'decrypt' ) {
+		$output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+	}
+	return $output;
+}
+
+function token_bin2hex($longitud) {
+	$token = bin2hex(openssl_random_pseudo_bytes(($longitud - ($longitud % 2)) / 2));
+	return $token;
 }
 
 ?>
