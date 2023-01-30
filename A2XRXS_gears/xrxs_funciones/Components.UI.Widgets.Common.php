3f7390109ff6ccb76a7bb24bc81c4908c97d799e
tenshi98 <54037366+tenshi98@users.noreply.github.com>
2023-01-30 08:33:18 -0400

    Creacion widget youtuve player
    Creacion del widget responsivo que permite poner el reproductor de youtuve en el sitio

--- A2XRXS_gears/xrxs_funciones/Components.UI.Widgets.Common.php
+++ A2XRXS_gears/xrxs_funciones/Components.UI.Widgets.Common.php
@@ -1534,6 +1534,29 @@ function widget_radio_player(){
 
 }
 /*******************************************************************************************************************/
+//Muestra los dias feriados del año
+function widget_youtube_player($link){
+
+	//Se genera recurso
+	$order   = 'https://www.youtube.com/watch?v=';
+	$replace = 'https://www.youtube.com/embed/';
+	$newstr  = str_replace($order, $replace, $link);
+
+	//Genero el cuerpo
+	$input = '
+	<style>
+		.yt_container {position: relative; width: 100%;height: 0;padding-bottom:56.25%; margin-bottom: 15px;;}
+		.yt_container .video {position: absolute;top: 0;left: 0;width: 100%;height: 100%;}
+	</style>
+	<div class="yt_container">
+		<iframe class="video" src="'.$newstr.'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+	';
+
+	//devuelvo cuerpo
+	return $input;
+}
+/*******************************************************************************************************************/
 
 
 ?>
