292e3963129a238a301f7e80f4c7ddd6fd54935e
Victor Reyes <tenshi98@gmail.com>
2024-07-30 09:51:37 -0400

    Cambio validacion rut
    Se cambia validador de rut debido a que no funciona en php 8

--- A2XRXS_gears/xrxs_funciones/Helpers.Functions.Data.Validations.php
+++ A2XRXS_gears/xrxs_funciones/Helpers.Functions.Data.Validations.php
@@ -28,12 +28,68 @@ if( ! defined('XMBCXRXSKGC')) {
 //Funcion
 function validarRut($Data) {
     //dato
-	$Rut = str_replace('.', '', $Data);
+	/*$Rut = str_replace('.', '', $Data);
     //ejecuto
 	if (preg_match('/^(\d{1,9})-((\d|k|K){1})$/',$Rut,$d)) {
         $s=1;$r=$d[1];for($m=0;$r!=0;$r/=10)$s=($s+$r%10*(9-$m++%6))%11;
         return chr($s?$s+47:75)==strtoupper($d[2]);
-    }
+    }*/
+
+	//elimino el punto
+	$rut = str_replace('.', '', $Data);
+
+	// Verifica que no esté vacio y que el string sea de tamaño mayor a 3 carácteres(1-9)
+	if ((empty($rut)) || strlen($rut) < 3) {
+		return false; //RUT vacío o con menos de 3 caracteres.
+	}
+
+	// Quitar los últimos 2 valores (el guión y el dígito verificador) y luego verificar que sólo sea
+	// numérico
+	$parteNumerica = str_replace(substr($rut, -2, 2), '', $rut);
+
+	if (!preg_match("/^[0-9]*$/", $parteNumerica)) {
+		return false; //La parte numérica del RUT sólo debe contener números.
+	}
+
+	$guionYVerificador = substr($rut, -2, 2);
+	// Verifica que el guion y dígito verificador tengan un largo de 2.
+	if (strlen($guionYVerificador) != 2) {
+		return false; //Error en el largo del dígito verificador.
+	}
+
+	// obliga a que el dígito verificador tenga la forma -[0-9] o -[kK]
+	if (!preg_match('/(^[-]{1}+[0-9kK]).{0}$/', $guionYVerificador)) {
+		return false; //El dígito verificador no cuenta con el patrón requerido
+	}
+
+	// Valida que sólo sean números, excepto el último dígito que pueda ser k
+	if (!preg_match("/^[0-9.]+[-]?+[0-9kK]{1}/", $rut)) {
+		return false; //Error al digitar el RUT
+	}
+
+	$rutV   = preg_replace('/[\.\-]/i', '', $rut);
+	$dv     = substr($rutV, -1);
+	$numero = substr($rutV, 0, strlen($rutV) - 1);
+	$i      = 2;
+	$suma   = 0;
+	foreach (array_reverse(str_split($numero)) as $v) {
+		if ($i == 8) {
+			$i = 2;
+		}
+		$suma += $v * $i;
+		++$i;
+	}
+	$dvr = 11 - ($suma % 11);
+	if ($dvr == 11) {$dvr = 0;}
+	if ($dvr == 10) {$dvr = 'K';}
+	if ($dvr == strtoupper($dv)) {
+		return true; //RUT ingresado correctamente.
+	} else {
+		return false; //El RUT ingresado no es válido.
+	}
+
+
+
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************
