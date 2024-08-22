000ada3c38be695031ff7088cbfd3f3cd2ca609e
Victor Reyes <tenshi98@gmail.com>
2024-08-22 15:51:42 -0400

    actualizacion libreria validator
    actualizacion libreria validator

--- LIBS_js/validator/form_validator.php
+++ LIBS_js/validator/form_validator.php
@@ -1,25 +1,9 @@
 <script>
-
-	// initialize the validator
 	var validator = new FormValidator();
-
-	// validate a field on "blur" event, a 'select' on 'change' event & a '.reuired' classed multifield on 'keyup':
-	/*$('form')
-		.on('blur', 'input[required], input.optional, select.required', function(){
-			validator.checkField.call(validator, this)
-		})
-		.on('change', 'select.required', function(){
-			validator.checkField.call(validator, this)
-		})
-		.on('keypress', 'input[required][pattern]', function(){
-			validator.checkField.call(validator, this)
-		})*/
-	// bind the validation to the form submit event
-	$('form').submit(function(e){
-		var submit = true,
-			validatorResult = validator.checkAll(this);
+	// select your "form" element from the DOM and attach an "onsubmit" event handler to it:
+	document.forms[0].onsubmit = function(e){
+		var validatorResult = validator.checkAll(this); // "this" reffers to the currently submitetd form element
 
 		return !!validatorResult.valid;
-	});
-
+	};
 </script>
